b2c01f864c9e5b965b2aebb64f3e334c7252b6a5
NastiaGusev <gusev.nastia@gmail.com>
2022-01-14 16:32:32 +0200

    Added ProgressBars with timers and ExerciseModel

---
+++ app/src/main/java/com/example/a7minutesworkout/Constants.kt
@@ -0,0 +1,117 @@
+package com.example.a7minutesworkout
+
+object Constants {
+
+    fun defaultExerciseList(): ArrayList<ExerciseModel> {
+        val exerciseModel = ArrayList<ExerciseModel>()
+
+        val jumpingJacks = ExerciseModel(
+            1,
+            "Jumping Jacks",
+            R.drawable.jumping_jacks,
+            false,
+            false
+        )
+        exerciseModel.add(jumpingJacks)
+
+        val jumpingSquats = ExerciseModel(
+            2,
+            "Jumping Squats",
+            R.drawable.jump_squats,
+            false,
+            false
+        )
+        exerciseModel.add(jumpingSquats)
+
+        val squatThrusts = ExerciseModel(
+            3,
+            "Squat Thrusts",
+            R.drawable.squat_thrust,
+            false,
+            false
+        )
+        exerciseModel.add(squatThrusts)
+
+        val switchKicks = ExerciseModel(
+            4,
+            "Switch Kicks",
+            R.drawable.switch_kicks,
+            false,
+            false
+        )
+        exerciseModel.add(switchKicks)
+
+        val beltKicks = ExerciseModel(
+            5,
+            "Belt Kicks",
+            R.drawable.belt_kicks,
+            false,
+            false
+        )
+        exerciseModel.add(beltKicks)
+
+        val buttKicks = ExerciseModel(
+            6,
+            "Butt Kicks",
+            R.drawable.butt_kicks,
+            false,
+            false
+        )
+        exerciseModel.add(buttKicks)
+
+        val highKnees = ExerciseModel(
+            7,
+            "High Knees",
+            R.drawable.high_knees,
+            false,
+            false
+        )
+        exerciseModel.add(highKnees)
+
+        val donkeyKicks = ExerciseModel(
+            8,
+            "Donkey Kicks",
+            R.drawable.donkey_kick,
+            false,
+            false
+        )
+        exerciseModel.add(donkeyKicks)
+
+        val birdDog = ExerciseModel(
+            9,
+            "Bird Dog",
+            R.drawable.bird_dog,
+            false,
+            false
+        )
+        exerciseModel.add(birdDog)
+
+        val crunches = ExerciseModel(
+            10,
+            "Crunches",
+            R.drawable.crunches,
+            false,
+            false
+        )
+        exerciseModel.add(crunches)
+
+        val boxJumps = ExerciseModel(
+            11,
+            "Box Jumps",
+            R.drawable.box_jumps,
+            false,
+            false
+        )
+        exerciseModel.add(boxJumps)
+
+        val tricepsDips = ExerciseModel(
+            12,
+            "Triceps Dips",
+            R.drawable.triceps_dips,
+            false,
+            false
+        )
+        exerciseModel.add(tricepsDips)
+        return exerciseModel
+    }
+}
--- app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
@@ -1,13 +1,25 @@
 package com.example.a7minutesworkout
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.Toast
 import com.example.a7minutesworkout.databinding.ActivityExerciseBinding
 import com.example.a7minutesworkout.databinding.ActivityMainBinding
 
 class ExerciseActivity : AppCompatActivity() {
 
     private var binding: ActivityExerciseBinding? = null
+
+    private var restProgress = 0
+    private var exerciseProgress = 0
+    private var restTimer: CountDownTimer? = null
+    private var exerciseTimer: CountDownTimer? = null
+
+    private var exerciseList: ArrayList<ExerciseModel>? = null
+    private var currentExercisePosition = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,13 +30,98 @@ class ExerciseActivity : AppCompatActivity() {
         //Set the action bar
         setSupportActionBar(binding?.toolBarExercise)
         //Activate back button
-        if(supportActionBar != null) {
+        if (supportActionBar != null) {
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
+        //Create the list of the exercises
+        exerciseList = Constants.defaultExerciseList()
 
         binding?.toolBarExercise?.setNavigationOnClickListener {
             onBackPressed()
         }
 
+        setUpRestView()
+    }
+
+    private fun setUpRestView() {
+        //change the view for the exercise
+        binding?.frameLayoutProgressbarRest?.visibility = View.VISIBLE
+        binding?.textviewTitle?.visibility = View.VISIBLE
+        binding?.frameLayoutProgressbarExercise?.visibility = View.INVISIBLE
+        binding?.textViewExerciseName?.visibility = View.INVISIBLE
+        binding?.exerciseImage?.visibility = View.INVISIBLE
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        setRestProgressBar()
+    }
+
+    private fun setUpExerciseView() {
+        //change the view for the exercise
+        binding?.frameLayoutProgressbarRest?.visibility = View.INVISIBLE
+        binding?.textviewTitle?.visibility = View.INVISIBLE
+        binding?.frameLayoutProgressbarExercise?.visibility = View.VISIBLE
+        binding?.textViewExerciseName?.visibility = View.VISIBLE
+        binding?.exerciseImage?.visibility = View.VISIBLE
+
+        //check if there is existing timer
+        if (exerciseTimer != null) {
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
+        }
+        //set the name and image for the current exercise
+        binding?.exerciseImage?.setImageResource(exerciseList!![currentExercisePosition].getImage())
+        binding?.textViewExerciseName?.text = exerciseList!![currentExercisePosition].getName()
+        setExerciseProgressBar()
+    }
+
+    private fun setRestProgressBar() {
+        binding?.progressbar?.progress = restProgress
+        restTimer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+                binding?.progressbar?.progress = 10 - restProgress
+                binding?.textviewTimer?.text = (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                //Go to the next exercise
+                currentExercisePosition++
+                setUpExerciseView()
+            }
+        }.start()
+    }
+
+    private fun setExerciseProgressBar() {
+        binding?.progressbarExercise?.progress = exerciseProgress
+        exerciseTimer = object : CountDownTimer(30000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                exerciseProgress++
+                binding?.progressbarExercise?.progress = 30 - exerciseProgress
+                binding?.textviewTimerExercise?.text = (30 - exerciseProgress).toString()
+            }
+
+            override fun onFinish() {
+                if(currentExercisePosition < exerciseList?.size!!-1){
+                    setUpRestView()
+                }else{
+                    Toast.makeText(this@ExerciseActivity,"Congrats you finished the workout!", Toast.LENGTH_LONG).show()
+                }
+            }
+        }.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        if (exerciseTimer != null) {
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
+        }
+        binding = null
     }
 }
